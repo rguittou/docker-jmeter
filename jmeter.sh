c7797edec6acaa0ab1b56767e666b55921682f9f
stagiaire <rabahguittoune@gmail.com>
2016-12-15 13:57:56 +0100

    script jmeter v3

--- jmeter.sh
+++ jmeter.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-apache-jmeter-3.1/bin/jmeter -JHOST=ip_flask_wb_server -JPORT=8080 -JNBR_USER=50  -Jjmeter.save.saveservice.output_format=xml  -Jjmeter.save.saveservice.response_data.on_error=true -n -t  csrf_token_user_defined_variable.jmx -l    ./results/web_server_result.jtl
+apache-jmeter-3.1/bin/jmeter -JHOST=$ip_flask_wb_server -JPORT=8080 -JNBR_USER=50  -Jjmeter.save.saveservice.output_format=xml  -Jjmeter.save.saveservice.response_data.on_error=true -n -t  csrf_token_user_defined_variable.jmx -l    ./results/web_server_result.jtl
